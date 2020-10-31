944321943b05f1613691bba38062d7c8a1c33156
Jan-Willem Gmelig Meyling <jan-willem@youngmediaexperts.nl>
2020-10-31 12:14:24 +0100

    [#2684] Extend example with QClass use in main code base

---
+++ querydsl-examples/querydsl-example-kotlin-mongodb/src/main/kotlin/com/querydsl/examples/kotlin/repository/ExampleUse.kt
@@ -0,0 +1,11 @@
+package com.querydsl.examples.kotlin.repository
+
+import com.querydsl.examples.kotlin.entity.QExampleEntity
+
+class ExampleUse {
+
+    fun test() {
+        val qExampleEntity = QExampleEntity("test");
+    }
+
+}
